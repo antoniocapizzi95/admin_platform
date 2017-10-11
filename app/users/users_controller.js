a79ab1c5cb19992a4cf4ab1e44394ccfe9cb6234
Antonio <ac2@nigithorconsulting.com>
2017-10-11 17:36:39 +0200

    add e delete uenti

--- app/users/users_controller.js
+++ app/users/users_controller.js
@@ -7,11 +7,49 @@
 
     angular.module('myApp.users', [])
         .controller('usersCtrl', usersCtrl);
-    usersCtrl.$inject = ['$scope'];
+    usersCtrl.$inject = ['$scope','$http','$route'];
 
-    function usersCtrl($scope) {
+    function usersCtrl($scope,$http,$route) {
 
+        $scope.users = [];
+        $scope.addUserButtonShow = true;
+        $scope.newUsername;
+        $scope.newPassword;
+        $http.get("http://localhost/mydb/getUsers.php")
+            .then(function (response) {
+                var input = JSON.parse(response.data);
+                $scope.users = input.records;
+            });
 
+        $scope.addUser = function () {
+            var param = JSON.stringify({username:$scope.newUsername,password:$scope.newPassword});
+
+            $http({
+                method: 'POST',
+                url: 'http://localhost/mydb/addUser.php',
+                data: "message=" + param,
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            })
+                .then(function (response) {
+                    $scope.addUserButtonShow = true;
+                    $route.reload();
+                });
+        }
+
+        $scope.deleteUser = function (id) {
+            var param = JSON.stringify({id:id});
+
+            $http({
+                method: 'POST',
+                url: 'http://localhost/mydb/deleteUser.php',
+                data: "message=" + param,
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            })
+                .then(function (response) {
+
+                    $route.reload();
+                });
+        }
 
 
 
